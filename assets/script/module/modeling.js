670b6da17ee0574c0a34d156d9774c1ea8f2fd47
niyorn <c.niyornram@hotmail.com>
2018-06-04 08:48:48 +0200

    Create basic plaine

--- assets/script/module/modeling.js
+++ assets/script/module/modeling.js
@@ -1,36 +1,34 @@
-let model = {
-    createCube: function () {
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer;
+let scene;
+let camera;
 
-        var renderer = new THREE.WebGLRenderer();
+let model = {
+    init : function () {
+        let scene = new THREE.Scene();
+    },
+    createCube: function () {
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight, 0.1, 1000);
+        camera.position.x = 15;
+        camera.position.y = 15;
+        camera.position.z = 15;
+        camera.lookAt(scene.position);
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor(0x000000, 1);
         renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMap.enabled = true;
+
         document.body.appendChild(renderer.domElement);
 
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({
-            color: 0x00ff00
-        });
-        var cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        camera.position.z = 5;
-
-        function animate() {
-            requestAnimationFrame(animate);
-
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            renderer.render(scene, camera);
-        }
-
-        animate();
-    },
-    test: function () {
-        let renderer;
-        let scene;
-        let camera;
+        render();
     }
+}
+
+function render () {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 
 export {model}
