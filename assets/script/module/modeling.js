3c940c9cbacc3b2b779a6fcfd0da6fe472e36a8d
niyorn <c.niyornram@hotmail.com>
2018-06-06 15:46:20 +0200

    Add rotation control

--- assets/script/module/modeling.js
+++ assets/script/module/modeling.js
@@ -1,46 +1,51 @@
 let renderer;
 let scene;
 let camera;
+let control;
+let stats;
+let object = {}
 
 let model = {
-    init : function () {
-        let scene = new THREE.Scene();
-    },
-    createCube: function () {
+    init: function () {
         //Create Scene
         scene = new THREE.Scene();
 
         //Create Camera
-        camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight, 0.1, 1000);
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.x = 15;
         camera.position.y = 35;
         camera.position.z = 30;
         camera.lookAt(scene.position);
-
+    },
+    createCube: function () {
         //Create object
-        let cubeGeometry = new THREE.CubeGeometry(7,4,7);
+        let cubeGeometry = new THREE.CubeGeometry(7, 4, 7);
         let cubeMaterial = new THREE.MeshLambertMaterial({
             color: 'red'
         })
+
         let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
         cube.castShadow = true;
+        cube.name = 'cube'
         scene.add(cube);
-
+        object.cube = cube;
         //Create plane for the object to sit on
         let planeGeometery = new THREE.PlaneGeometry(20, 20);
         let planeMaterial = new THREE.MeshLambertMaterial({
             color: 'blue'
         });
-        let plane = new THREE.Mesh(planeGeometery,planeMaterial);
+        let plane = new THREE.Mesh(planeGeometery, planeMaterial);
         plane.rotation.x = -0.5 * Math.PI;
         plane.position.y = -4;
-        plane.receiveShadow = true;
+        plane.receiveShadow = true;;
+        plane.name = 'plane'
         scene.add(plane);
 
         //Shine light on object
         var spotLight = new THREE.SpotLight(0xffffffff);
         spotLight.position.set(10, 20, 20);
         spotLight.castShadow = true;
+        spotLight.name = 'spotLight'
         scene.add(spotLight);
 
         //Render 
@@ -51,40 +56,40 @@ let model = {
 
         document.body.appendChild(renderer.domElement);
 
+        this.addControl()
+
         render();
-        
-        function animate (){
-            requestAnimationFrame( animate );
 
-            cube.rotation.x += 0.1;
-            cube.rotation.y += 0.1;
-
-            renderer.render( scene, camera );
-        };
-
-        animate();
-
-        //Control
-        let control = new function() {
+        // animate();
+    },
+    addControl: function () {
+        control = new function () {
             this.rotationSpeed = 0.005;
             this.opacity = 0.6;
-            this.color = cubeMaterial.color.getHex();
-            };
-
+        };
         addControlGui(control);
-
-        function addControlGui(controlObject) {
-            var gui = new dat.GUI();
-            gui.add(controlObject, 'rotationSpeed', -0.01, 0.01);
-            gui.add(controlObject, 'opacity', 0.1, 1);
-            gui.addColor(controlObject, 'color');
-            }
     }
 }
 
-function render () {
-    requestAnimationFrame(render);
+
+function animate() {
+    requestAnimationFrame(animate);
+    object.cube.rotation.y += 0.1;
     renderer.render(scene, camera);
 }
 
-export {model}
+function render() {
+    requestAnimationFrame(render);
+    object.cube.rotation.y += control.rotationSpeed;
+    renderer.render(scene, camera);
+}
+
+function addControlGui(controlObject) {
+    var gui = new dat.GUI();
+    gui.add(controlObject, 'rotationSpeed', -0.25, 0.25);
+}
+
+
+export {
+    model
+}
