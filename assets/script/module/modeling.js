df3707f6aa6573fdd92bd4f56da2f3aeb17648a7
niyorn <c.niyornram@hotmail.com>
2018-06-09 14:13:37 +0200

    add book model

--- assets/script/module/modeling.js
+++ assets/script/module/modeling.js
@@ -2,6 +2,7 @@ let renderer;
 let scene;
 let camera;
 let control;
+let controls;
 let stats;
 let object = {}
 
@@ -11,48 +12,48 @@ let model = {
         scene = new THREE.Scene();
 
         //Create Camera
-        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.x = 15;
-        camera.position.y = 35;
-        camera.position.z = 30;
+        camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.x = 0;
+        camera.position.y = 4;
+        camera.position.z = 3;
         camera.lookAt(scene.position);
+        let modelPath = '../assets/model/model.gltf'
+
+        this.loader(modelPath)
     },
     createCube: function () {
         //Create object
-        let cubeGeometry = new THREE.CubeGeometry(7, 4, 7);
-        let cubeMaterial = new THREE.MeshLambertMaterial({
-            color: 'red'
-        })
+        // let cubeGeometry = new THREE.CubeGeometry(7, 4, 7);
+        // let cubeMaterial = new THREE.MeshLambertMaterial({
+        //     color: 'red'
+        // })
 
-        let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        cube.castShadow = true;
-        cube.name = 'cube'
-        scene.add(cube);
-        object.cube = cube;
+        // let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        // cube.castShadow = true;
+        // cube.name = 'cube'
+        // scene.add(cube);
+        // object.cube = cube;
         //Create plane for the object to sit on
-        let planeGeometery = new THREE.PlaneGeometry(20, 20);
-        let planeMaterial = new THREE.MeshLambertMaterial({
-            color: 'blue'
-        });
-        let plane = new THREE.Mesh(planeGeometery, planeMaterial);
-        plane.rotation.x = -0.5 * Math.PI;
-        plane.position.y = -4;
-        plane.receiveShadow = true;;
-        plane.name = 'plane'
-        scene.add(plane);
+        // let planeGeometery = new THREE.PlaneGeometry(30, 30);
+        // let planeMaterial = new THREE.MeshLambertMaterial({
+        //     color: 'blue'
+        // });
+        // let plane = new THREE.Mesh(planeGeometery, planeMaterial);
+        // plane.rotation.x = -0.5 * Math.PI;
+        // plane.position.y = -10;
+        // plane.receiveShadow = true;;
+        // plane.name = 'plane'
+        // scene.add(plane);
 
         //Shine light on object
-        var spotLight = new THREE.SpotLight(0xffffffff);
-        spotLight.position.set(10, 20, 20);
-        spotLight.castShadow = true;
-        spotLight.name = 'spotLight'
-        scene.add(spotLight);
-
+        var light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
+        light.position.set( 1, 1, 1 );
+        scene.add(light);       
+        
         //Render 
         renderer = new THREE.WebGLRenderer();
-        renderer.setClearColor(0x000000, 1);
+        renderer.setClearColor(0xffffff, 1);
         renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.shadowMap.enabled = true;
 
         document.body.appendChild(renderer.domElement);
 
@@ -60,6 +61,10 @@ let model = {
         this.addStats()
 
         this.render();
+
+        controls = new THREE.OrbitControls( camera );
+        controls.update()
+
     },
     addControl: function () {
         control = new function () {
@@ -68,27 +73,46 @@ let model = {
         };
         addControlGui(control);
     },
-    render : function () {
+    render: function () {
         requestAnimationFrame(this.render.bind(this));
-        object.cube.rotation.y += control.rotationSpeed;
         stats.update();
         renderer.render(scene, camera);
     },
-    addStats : function () {
+    addStats: function () {
         stats = new Stats();
         stats.setMode(0);
         stats.domElement.style.position = 'absolute';
         stats.domElement.style.left = '0px';
         stats.domElement.style.top = '0px';
-        document.body.appendChild( stats.domElement );
+        document.body.appendChild(stats.domElement);
+    },
+    loader: function (modelPath) {
+        let loader = new THREE.GLTFLoader();
+        // Load a glTF resource
+        console.log(modelPath)
+        loader.load(modelPath, function (gltf) {
+                scene.add(gltf.scene);
+
+            },
+            // called when loading is in progresses
+            function (xhr) {
+
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+            },
+            // called when loading has errors
+            function (error) {
+                console.log('An error happened', error);
+
+            }
+        );
     }
 }
 
 
 function animate() {
     requestAnimationFrame(animate);
-    object.cube.rotation.y += 0.1;
-    renderer.render(scene, camera);
+    camera.rotation.y += 0.04;
 }
 
 function addControlGui(controlObject) {
@@ -100,4 +124,3 @@ function addControlGui(controlObject) {
 export {
     model
 }
-
